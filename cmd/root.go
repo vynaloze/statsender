f12f8d84cc3d2fa9ce71206d089920241bcdbc6f
Piotr Pawluk <vynaloze@gmail.com>
2019-05-28 20:52:33 +0200

    set log level through CLI

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/vynaloze/statsender/logger"
 	"os"
 )
 
@@ -11,16 +12,22 @@ var rootCmd = &cobra.Command{Use: "statsender"}
 var verbose bool
 var configDir string
 
-func Execute() {
+func init() {
+	cobra.OnInitialize(initConfig)
 	rootCmd.Version = "0.0.1"
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
 	rootCmd.PersistentFlags().StringVarP(&configDir, "config", "c", "/etc/statsender/conf.d", "sets configuration directory location")
-
 	addRunning()
 	addDs()
+}
 
+func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func initConfig() {
+	logger.SetDebug(verbose)
 }
