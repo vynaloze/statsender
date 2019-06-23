34b49a55c4f789827b333fadf65667147ce5beca
Piotr Pawluk <vynaloze@gmail.com>
2019-06-23 12:36:45 +0200

    pg_stat_user_indexes (again)

--- collector/pg_stat_user_indexes.go
+++ collector/pg_stat_user_indexes.go
@@ -1,11 +1,20 @@
 package collector
 
 import (
+	"github.com/vynaloze/pgstats/nullable"
 	"github.com/vynaloze/statsender/dto"
 	"github.com/vynaloze/statsender/logger"
 )
 
 type PgStatUserIndexes Config
+
+type pgStatUserIndexesPayload struct {
+	Table       string         `json:"table"`
+	Index       string         `json:"index"`
+	IdxScan     nullable.Int64 `json:"idx_scan"`
+	IdxTupRead  nullable.Int64 `json:"idx_tup_read"`
+	IdxTupFetch nullable.Int64 `json:"idx_tup_fetch"`
+}
 
 func (p *PgStatUserIndexes) Collect(datasource *Datasource) *dto.Stat {
 	log, _ := logger.New()
@@ -14,8 +23,18 @@ func (p *PgStatUserIndexes) Collect(datasource *Datasource) *dto.Stat {
 	if err != nil {
 		log.Error(err)
 	}
+	var payload []pgStatUserIndexesPayload
+	for _, r := range s {
+		payload = append(payload, pgStatUserIndexesPayload{
+			r.Schemaname + "." + r.Relname,
+			r.Indexrelname,
+			r.IdxScan,
+			r.IdxTupRead,
+			r.IdxTupFetch,
+		})
+	}
 
-	return dto.NewStat(datasource.DsDto, "pg_stat_user_indexes", s)
+	return dto.NewStat(datasource.DsDto, "pg_stat_user_indexes", payload)
 }
 
 func (p *PgStatUserIndexes) Conf() Config {
